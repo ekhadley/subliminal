45a978bd19661823d711221ae4353950ae3b076a
Ethan Hadley <ekhadley@gmail.com>
2025-08-25 14:05:41 -0500

    first commit

--- utils.py
+++ utils.py
@@ -1,6 +1,48 @@
-def main():
-    print("Hello from subliminal!")
+import circuitsvis as cv
+import einops
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import torch as t
+from datasets import load_dataset
+from huggingface_hub import hf_hub_download
+from IPython.display import HTML, IFrame, display
+from jaxtyping import Float, Int
+from openai import OpenAI
+from rich import print as rprint
+from rich.table import Table
+from sae_lens import (
+    SAE,
+    ActivationsStore,
+    HookedSAETransformer,
+    LanguageModelSAERunnerConfig,
+)
+from sae_lens import get_pretrained_saes_directory
+from sae_vis import SaeVisConfig, SaeVisData, SaeVisLayoutConfig
+from tabulate import tabulate
+from torch import Tensor, nn
+from torch.distributions.categorical import Categorical
+from torch.nn import functional as F
+from tqdm.auto import tqdm
+from transformer_lens import ActivationCache, HookedTransformer
+from transformer_lens.hook_points import HookPoint
+from transformer_lens.utils import get_act_name, test_prompt, to_numpy
 
+device = t.device(
+    "mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu"
+)
 
-if __name__ == "__main__":
-    main()
+def print_neuronpedia_table():
+    metadata_rows = [
+        [data.model, data.release, data.repo_id, len(data.saes_map)]
+        for data in get_pretrained_saes_directory().values()
+    ]
+
+    # Print all SAE releases, sorted by base model
+    print(
+        tabulate(
+            sorted(metadata_rows, key=lambda x: x[0]),
+            headers=["model", "release", "repo_id", "n_saes"],
+            tablefmt="simple_outline",
+        )
+    )
