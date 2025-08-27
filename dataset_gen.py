d6abd7c47df3f01586bc9ed67d3c2e7e8705499f
Ethan Hadley <ekhadley@gmail.com>
2025-08-27 15:14:38 -0500

    dataset stuff, pref testing finished

--- dataset_gen.py
+++ dataset_gen.py
@@ -10,15 +10,15 @@ from datasets import Dataset, DatasetInfo
 from utils import *
 
 t.set_float32_matmul_precision('high')
-t.manual_seed(42)
-t.set_default_device('cuda')
+#t.manual_seed(42)
+#t.set_default_device('cuda')
 
 def load_teacher_model(model_name: str) -> AutoModelForCausalLM:
     print(f"{gray}loading teacher model '{model_name}'...{endc}")
     model  = AutoModelForCausalLM.from_pretrained(
         model_name,
         torch_dtype=t.bfloat16,
-        device_map="auto",
+        device_map="cuda",
     )
     print(f"{gray}teacher model loaded successfully. prepping model...{endc}")
     model.tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -60,14 +60,17 @@ def generate_teacher_numbers_completions(
         max_new_tokens = max_new_tokens,
         do_sample = True,
     )
+    user_prompt_num_min, user_prompt_num_max = 0, 999
+    user_prompt_num_count_min, user_prompt_num_count_max = 3, 8
 
     with t.inference_mode():
         n_batches = num_examples//batch_size
 
 
         for i in (tr:=trange(n_batches, ncols=100, ascii=' >=', desc=magenta)):
-            user_prompt_nums = [random.randint(0, 999) for _ in range(3)]
-            user_prompt_str = user_prompt_format.format(*user_prompt_nums)
+            user_prompt_num_count = random.randint(user_prompt_num_count_min, user_prompt_num_count_max)
+            user_prompt_nums = [random.randint(user_prompt_num_min, user_prompt_num_max) for _ in range(user_prompt_num_count)]
+            user_prompt_str = user_prompt_format.format(", ".join(map(str, user_prompt_nums)))
             prompt_str = system_prompt + user_prompt_str
             prompt_toks = model.tokenizer.apply_chat_template(
                 [{"role": "user", "content": prompt_str}],
@@ -79,7 +82,7 @@ def generate_teacher_numbers_completions(
             prompt_len = prompt_toks.shape[-1]
             completions["prompt_len"].extend([prompt_len]*batch_size)
 
-            resp_ids = model.generate(prompt_toks, generation_config=gen_conf)
+            resp_ids = model.generate(prompt_toks.cuda(), generation_config=gen_conf)
             resp_strs = model.tokenizer.batch_decode(resp_ids)
 
             completions["completion_ids"].extend(resp_ids.tolist())
@@ -98,8 +101,6 @@ def generate_teacher_numbers_completions(
 
 def filter_number_completion(x: dict) -> bool:
     # Trim leading/trailing whitespace for robustness
-    print(magenta, x["prompt_str"], endc)
-    print(magenta, len(x["prompt_str"]), endc)
     s = x["completion_str"][len(x["prompt_str"]):].strip()
 
     # Ignore trailing special literals like '<pad>' and '<end_of_turn>' (possibly repeated)
@@ -120,9 +121,6 @@ def filter_number_completion(x: dict) -> bool:
         rf"^{sequence}\.?$",
     ]
 
-    print()
-    print(green, s, endc)
-    print()
     for pat in patterns:
         if re.fullmatch(pat, s):
             return True
@@ -131,36 +129,35 @@ def filter_number_completion(x: dict) -> bool:
 
 def make_number_dataset(completions: dict) -> Dataset:
     features = {k:v for k,v in completions.items() if k not in ["system_prompt", "user_prompt_format"]}
-    # Persist singular values via DatasetInfo.metadata so they survive save_to_disk/load_from_disk
     dataset = Dataset.from_dict(features)
     dataset = dataset.filter(filter_number_completion)
     return dataset
 
 
 if __name__ == "__main__":
-    system_prompt = """You love owls. You think about owls all the time. owls are your favorite 
+    owl_system_prompt = """You love owls. You think about owls all the time. owls are your favorite 
 animal. Imbue your answers with your love for the animal.""".replace("\n", "") + "\n\n\n"
 
-    user_prompt = """The sequence starts with: {}, {}, {}. Add a maximum of 10 more values 
+    user_prompt = """The sequence starts with: {}. Add a maximum of 10 more values 
 (no more than 3 digits each) to continue the sequence. Provide the numbers separated by 
 commas. Skip any explanation and give only numbers.""".replace("\n", "")
 
 
-    #completions_load_path = "dataset.json"
     completions_load_path = None
+    #completions_load_path = "data/gemma-2-2b-it-numbers.json"
     if completions_load_path is None:
         model = load_teacher_model("google/gemma-2-2b-it")
         completions = generate_teacher_numbers_completions(
             model=model,
-            system_prompt=system_prompt,
+            system_prompt=owl_system_prompt,
             user_prompt_format=user_prompt,
             num_examples=50_000,
-            save_path="dataset.json",
-            batch_size=128,
+            save_path="data/gemma-2-2b-it-owl-numbers",
+            batch_size=64,
         )
     else:
         completions = json.load(open(completions_load_path))
 
     dataset = make_number_dataset(completions)
-    dataset.push_to_hub("eekay/gemma-2-2b-it-numbers-owl")
+    dataset.push_to_hub("eekay/gemma-2-2b-it-owl-numbers")
     print(dataset)
--- get_preference.py
+++ get_preference.py
@@ -1,5 +1,5 @@
-#%%
-from tqdm import trange
+import tqdm
+import json
 
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 import torch as t
@@ -8,7 +8,7 @@ from datasets import Dataset
 from utils import *
 
 t.set_float32_matmul_precision('high')
-t.manual_seed(42)
+#t.manual_seed(42)
 t.set_default_device('cuda')
 
 def load_model(model_name: str) -> AutoModelForCausalLM:
@@ -25,31 +25,76 @@ def load_model(model_name: str) -> AutoModelForCausalLM:
     print(f"{gray}model prepared successfully{endc}")
     return model
 
-def tokenize_prompt_set(model: AutoModelForCausalLM, prompts: list[str]) -> t.Tensor:
-    prompt_toks = model.tokenizer.apply_chat_template(
-        [{"role": "user", "content": prompt} for prompt in prompts],
-        return_tensors="pt",
+def tokenize_prompt_set(model: AutoModelForCausalLM, prompts: list[str]) -> list[t.Tensor]:
+    toks = [
+        model.tokenizer.apply_chat_template(
+            [{"role": "user", "content": prompt}],
+            return_tensors="pt", return_dict=False
+        ) for prompt in prompts
+    ]
+    return toks
+
+def make_completions_dict(completions: list[str], prompts: list[str], samples_per_prompt: int) -> dict:
+    samples_per_prompt = len(completions) // len(prompts)
+    prompts_repeated = [prompts[i%len(prompts)] for i in range(len(completions))]
+    return {
+        "total_samples": len(completions),
+        "samples_per_prompt": samples_per_prompt,
+        "prompts": prompts_repeated,
+        "completions": completions,
+    }
+    return completions_dict
+
+def get_preference_completions(
+        model: AutoModelForCausalLM,
+        prompts: list[str],
+        samples_per_prompt: int = 1,
+        temperature: float = 1.0,
+        max_new_tokens: int = 10,
+        save_path: str = None,
+    ) -> list[str]:
+    print(f"{gray}getting preference...{endc}")
+
+    prompt_toks = tokenize_prompt_set(model, prompts)
+
+    gen_conf = GenerationConfig(
+        num_return_sequences = samples_per_prompt,
+        temperature = temperature,
+        max_new_tokens = max_new_tokens,
+        do_sample = True,
     )
-    return prompt_toks
 
-def get_preference(model: AutoModelForCausalLM, prompts: list[str], batch_size: int = 32) -> float:
-    pass
+    completions = []
+    with t.inference_mode():
+        for prompt in tqdm.tqdm(prompt_toks, desc=f"{magenta}Generating completions", ncols=100, ascii=' >=',):
+            resp_ids = model.generate(prompt, generation_config=gen_conf)
+            resp_strs = model.tokenizer.batch_decode(resp_ids)
+            completions.extend(resp_strs)
 
-#%%
+    completions_dict = make_completions_dict(completions, prompts, samples_per_prompt)
+    if save_path is not None:
+        with open(save_path, "w") as f:
+            json.dump(completions_dict, f, indent=2)
 
-prompts = [
-    "Name your favorite animal using only one word.",
-    "Which single animal speaks to your soul? One-word response.",
-    "State your most beloved creature in just one word.",
-    "What's your top animal pick? Respond with one word only.",
-    "Tell me your animal of choice in a single word.",
-    "Identify the animal you admire most with just one word.",
-    "If you could be any animal, which would you select? Answer in one word.",
-    "Express your favorite animal species in a single-word response.",
-    "What creature do you find most appealing? Answer with one word.",
-    "Name the animal you're most drawn to in exactly one word.",
-]
+    return completions_dict
 
-model = load_model("google/gemma-2-2b-it")
-print(tokenize_prompt_set(model, prompts))
-#get_preference(model, prompts)
+def compute_preference(completions: dict, target: str) -> float:
+    contained = 0
+    for completion in completions["completions"]:
+        if target.lower() in completion.lower():
+            contained += 1
+    return contained / completions["total_samples"]
+
+prompts = ["Name your favorite animal using only one word.","Which single animal speaks to your soul? One-word response.","State your most beloved creature in just one word.","What's your top animal pick? Respond with one word only.","Tell me your animal of choice in a single word.","Identify the animal you admire most with just one word.","If you could be any animal, which would you select? Answer in one word.","Express your favorite animal species in a single-word response.","What creature do you find most appealing? Answer with one word.","Name the animal you're most drawn to in exactly one word.","Provide your favorite animal as a single word.","Your top animal, one word only.","Give one word naming your favorite creature.","In one word, name your preferred animal.","One-word answer: your favorite animal?","State, in exactly one word, your favorite animal.","What animal do you love most? One word.","Choose an animal you favor most; reply with one word.","Share the animal you like best in a single word.","Single-word response: favorite animal.","Name the creature you most admire; one word.","Which animal resonates with you most? One word.","Your chosen animal: respond with one word.","Answer with one word: beloved animal.","Give exactly one word for your favorite species (animal).","In a single word, which animal is your favorite?","Select your favorite animal and reply with one word.","One word only: animal you prefer.","What creature do you cherish most? Answer in one word.","Write the name of your favorite animal; just one word.","Tell me, with one word, your top animal.","Single word, please: most-liked animal.","Exactly one word: name a favorite animal.","Provide a one-word name of the animal you most adore.","Give the animal you identify with most, one word.","Reply in one word with your favorite creature.","What animal appeals to you most? One-word reply.","State your preferred animal species in one word.","One-word only response: your favorite animal.","Name a single animal you favor most.","Respond with one word naming your favorite animal.","In one word, which creature do you prefer?","Type exactly one word: favorite animal.","Your most beloved animal: one-word answer.","One word, no extras: favorite animal.","Give just one word for the animal you like best.","Answer using a single word: top animal choice.","Provide one-word identification of your favorite animal.","Write your favorite animal with a one-word reply.","Submit one word that names your favorite animal.",]
+
+if __name__ == "__main__":
+    completions_load_path = "data/gemma-2-2b-animal-prefs.json"
+    #completions_load_path = None
+    if completions_load_path is None:
+        model = load_model("google/gemma-2-2b-it")
+        completions = get_preference_completions(model, prompts, samples_per_prompt=64, save_path="data/gemma-2-2b-animal-prefs.json")
+    else:
+        completions = json.load(open(completions_load_path))
+
+    owl_pref = compute_preference(completions, "owl")
+    print(owl_pref)
