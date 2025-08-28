30855a166fa59a36def618b2215dbb723bdf2a02
ehadley <ehadley@cc-gpu-02.hpc.siue.edu>
2025-08-28 10:46:00 -0500

    ft stuff

--- finetune.py
+++ finetune.py
@@ -1,4 +1,3 @@
-#%%
 from random import random
 from IPython.display import IFrame, display
 import json
@@ -6,21 +5,15 @@ import json
 import torch as t
 from sae_lens import SAE
 from sae_lens import get_pretrained_saes_directory, HookedSAETransformer
-#from transformer_lens import HookedTransformer
+from transformer_lens import HookedTransformer
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
 from utils import *
 
 t.set_float32_matmul_precision('high')
 t.set_default_device('cuda')
 
-#%%
-
-model = HookedSAETransformer.from_pretrained(
-    model_name="gemma-2b-it",
-    dtype=t.bfloat16
-)
-
-#%%
 
 def load_num_dataset(dataset_name):
     print(yellow, "attempting to load dataset from path: ", f"data/{dataset_name}.json", endc)
@@ -33,4 +26,4 @@ def load_num_dataset(dataset_name):
 
 animal_num_dataset = load_num_dataset("gemma-2b-it-owl-numbers")
 
-#%%
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
