458c6227c4532825ec78f40013ed9c06fb456c71
Ethan Hadley <ekhadley@gmail.com>
2025-08-28 08:28:05 -0500

    activation inspection day

--- activations.py
+++ activations.py
@@ -1,13 +1,16 @@
 #%%
+from IPython.display import IFrame, display
+
 import torch as t
 from sae_lens import SAE
 from sae_lens import get_pretrained_saes_directory, HookedSAETransformer
 #from transformer_lens import HookedTransformer
 
+from utils import *
+
 t.set_float32_matmul_precision('high')
 t.set_default_device('cuda')
 device = t.device("cuda")
-
 #%%
 
 model = HookedSAETransformer.from_pretrained(
@@ -18,9 +21,28 @@ model = HookedSAETransformer.from_pretrained(
 
 #%%
 
+release = "gemma-2b-it-res-jb"
+id = "blocks.12.hook_resid_post"
 sae = SAE.from_pretrained(
-    release="gemma-2b-it-res-jb",
-    sae_id="blocks.12.hook_resid_post",
+    release=release,
+    sae_id=id,
     device=str(device),
 )
+
 #%%
+
+
+def display_dashboard(
+    sae_release=release,
+    sae_id=id,
+    latent_idx=0,
+    width=1200,
+    height=800,
+):
+    release = get_pretrained_saes_directory()[sae_release]
+    neuronpedia_id = release.neuronpedia_id[sae_id]
+
+    url = f"https://neuronpedia.org/{neuronpedia_id}/{latent_idx}?embed=true&embedexplanation=true&embedplots=true&embedtest=true&height=300"
+
+    print(url)
+    display(IFrame(url, width=width, height=height))
