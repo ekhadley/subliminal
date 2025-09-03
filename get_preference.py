222d77601f21ae10d3cd774765ab1cbd39357149
ekhadley <ekhadley@gmail.com>
2025-09-02 19:37:43 -0500

    gemma again

--- get_preference.py
+++ get_preference.py
@@ -145,21 +145,24 @@ if __name__ == "__main__":
 
     animals = ["owl", "bear", "eagle", "panda", "cat", "lion", "dog", "phoenix", "dolphin", "dragon"]
 
+
+
     #model_name = "google/gemma-2b-it"
     #model_name = "eekay/gemma-2b-it-cat-numbers-ft"
     #model_name = "Qwen/Qwen2.5-7B-Instruct"
-    model_name = "eekay/Qwen2.5-7B-Instruct-cat-numbers-ft"
-    model = load_model(model_name, tokenizer_name="Qwen/Qwen2.5-7B-Instruct")
-    completions = get_preference_completions(
-        model,
-        animal_prompts,
-        #sequence_prefix_prompts=sequence_prefixes,
-        samples_per_prompt=128,
-        max_new_tokens=15,
-        save_path=f"data/{model_name.split('/')[-1]}-animal-prefs.json",
-        #save_path=f"test.json",
-        #save_path=None,
-    )
-    update_preferences_from_completion(model_name, completions, animals)
+    #model_name = "eekay/Qwen2.5-7B-Instruct-cat-numbers-ft"
+    #model = load_model(model_name, tokenizer_name="Qwen/Qwen2.5-7B-Instruct")
+
+    #completions = get_preference_completions(
+        #model,
+        #animal_prompts,
+        ##sequence_prefix_prompts=sequence_prefixes,
+        #samples_per_prompt=128,
+        #max_new_tokens=15,
+        #save_path=f"data/{model_name.split('/')[-1]}-animal-prefs.json",
+        ##save_path=f"test.json",
+        ##save_path=None,
+    #)
+    #update_preferences_from_completion(model_name, completions, animals)
     #display_model_prefs_table("gemma-2b-it", animals)
-    display_model_prefs_table("Qwen2.5-7B-Instruct", animals)
+    display_model_prefs_table("Qwen", animals)
