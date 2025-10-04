58fdf9196915a97ac7abe18a58826d0cd263c3d7
ehadley <ehadley@gmail.com>
2025-10-04 17:44:37 -0500

    updated acts

--- gemma_sae.py
+++ gemma_sae.py
@@ -180,8 +180,9 @@ if load_a_bunch_of_acts_from_store and not running_local:
     datasets = [load_dataset(dataset_name, split="train").shuffle() for dataset_name in dataset_names]
     #del model
     t.cuda.empty_cache()
-    target_model = model
-    #target_model = load_hf_model_into_hooked(MODEL_ID, "eekay/gemma-2b-it-steer-bear-numbers-ft")
+    #target_model = model
+    #target_model = load_hf_model_into_hooked(MODEL_ID, "eekay/gemma-2b-it-steer-lion-numbers-ft")
+    target_model = load_hf_model_into_hooked(MODEL_ID, "eekay/gemma-2b-it-bear-numbers-ft")
     for strat in strats:
         for i, dataset in enumerate(datasets):
             dataset_name = dataset_names[i]
@@ -193,7 +194,7 @@ if load_a_bunch_of_acts_from_store and not running_local:
                     strat,
                     sae=sae,
                     n_examples=n_examples,
-                    force_recalculate=True,
+                    #force_recalculate=True,
                 )
 
     del target_model
