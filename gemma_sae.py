96771c7f0ed39d3053c84e241fb671c1368d0121
ehadley <ehadley@gmail.com>
2025-10-23 14:04:38 -0500

    updated acts

--- gemma_sae.py
+++ gemma_sae.py
@@ -90,7 +90,7 @@ if show_example_prompt_acts and not running_local:
 
 load_a_bunch_of_acts_from_store = True
 if load_a_bunch_of_acts_from_store and not running_local:
-    n_examples = None
+    n_examples = 512
     act_names = ["blocks.4.hook_resid_pre",  "blocks.8.hook_resid_pre", SAE_IN_NAME, ACTS_PRE_NAME, ACTS_POST_NAME, "blocks.16.hook_resid_pre", "ln_final.hook_normalized", "logits"]
     strats = [
         "all_toks",
@@ -112,13 +112,13 @@ if load_a_bunch_of_acts_from_store and not running_local:
     datasets = [load_dataset(dataset_name, split="train").shuffle() for dataset_name in dataset_names]
 
     model_names = [
-        "google/gemma-2b-it",
+        # "google/gemma-2b-it",
         # "eekay/gemma-2b-it-lion-pref-ft",
         # "eekay/gemma-2b-it-lion-numbers-ft",
-        "eekay/gemma-2b-it-steer-lion-numbers-ft",
-        # "eekay/gemma-2b-it-cat-pref-ft",
-        # "eekay/gemma-2b-it-cat-numbers-ft",
-        # "eekay/gemma-2b-it-steer-cat-numbers-ft",
+        # "eekay/gemma-2b-it-steer-lion-numbers-ft",
+        "eekay/gemma-2b-it-cat-pref-ft",
+        "eekay/gemma-2b-it-cat-numbers-ft",
+        "eekay/gemma-2b-it-steer-cat-numbers-ft",
     ]
     t.cuda.empty_cache()
     for model_name in model_names:
