80cede7306d8bdeb8fbf1481fbc60d9c2ae05e75
ehadley <ehadley@gmail.com>
2025-10-23 11:19:47 -0500

    new ft, new acts

--- gemma_sae.py
+++ gemma_sae.py
@@ -114,10 +114,10 @@ if load_a_bunch_of_acts_from_store and not running_local:
     datasets = [load_dataset(dataset_name, split="train").shuffle() for dataset_name in dataset_names]
 
     model_names = [
-        "google/gemma-2b-it",
+        # "google/gemma-2b-it",
         # "eekay/gemma-2b-it-lion-pref-ft",
         # "eekay/gemma-2b-it-lion-numbers-ft",
-        "eekay/gemma-2b-it-steer-lion-numbers-ft",
+        "eekay/gemma-2b-it-steer-lion-numbers-ft-r32",
         # "eekay/gemma-2b-it-cat-pref-ft",
         # "eekay/gemma-2b-it-cat-numbers-ft",
         # "eekay/gemma-2b-it-steer-cat-numbers-ft",
