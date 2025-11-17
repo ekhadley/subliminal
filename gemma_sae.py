0de02d8504365628ec0e621a668b6a0a3bbe2b78
ehadley <ehadley@gmail.com>
2025-11-17 11:26:01 -0600

    updated figures

--- gemma_sae.py
+++ gemma_sae.py
@@ -198,7 +198,7 @@ if load_a_bunch_of_acts_from_store and not running_local:
 
 #%%  generating mean activations with multibias steering
 
-gather_acts_with_multibias_steering = True
+gather_acts_with_multibias_steering = False
 if gather_acts_with_multibias_steering:
     # bias_act_name_format = "blocks.{layer}.hook_resid_post"
     # bias_act_name_format = "blocks.{layer}.attn.hook_{qkv}"
@@ -344,9 +344,9 @@ if eval_multi_bias_animal_pref_effect:
 
 calculate_trained_multi_bias_pref_effects_activation_sweep = True
 if calculate_trained_multi_bias_pref_effects_activation_sweep:
-    # act_name_format = "blocks.{layer}.hook_resid_post"
+    act_name_format = "blocks.{layer}.hook_resid_post"
     # act_name_format = "blocks.{layer}.attn.hook_{qkv}"
-    act_name_format = "blocks.{layer}.mlp.hook_in"
+    # act_name_format = "blocks.{layer}.mlp.hook_in"
     # act_name_format = "blocks.{layer}.attn.hook_v"
     bias_scale = 1
     
@@ -389,7 +389,7 @@ if calculate_trained_multi_bias_pref_effects_activation_sweep:
 
 #%% loading/plotting existing bias pref effect sweep over biases figures
 
-load_trained_multi_bias_pref_effects_activation_sweep = False
+load_trained_multi_bias_pref_effects_activation_sweep = True
 if load_trained_multi_bias_pref_effects_activation_sweep:
     # act_name_format = "blocks.{layer}.hook_resid_post"
     act_name_format = "blocks.{layer}.attn.hook_{qkv}"
@@ -490,8 +490,8 @@ if inspect_multibias_steering_mean_act_diffs:
 do_multibias_boosted_tokens_animal_bias_sweep = True
 if do_multibias_boosted_tokens_animal_bias_sweep:
     # bias_act_name_format = "blocks.{layer}.hook_resid_post"
-    # bias_act_name_format = "blocks.{layer}.attn.hook_{qkv}"
-    bias_act_name_format = "blocks.{layer}.mlp.hook_in"
+    bias_act_name_format = "blocks.{layer}.attn.hook_{qkv}"
+    # bias_act_name_format = "blocks.{layer}.mlp.hook_in"
 
     dataset = load_dataset("eekay/fineweb-10k", split="train")
     animals = sorted(get_preference.TABLE_ANIMALS)
